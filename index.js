3deea4a015c0d27cae9220197915f2b0557f0028
Christian Horban <116985183+Christian-Horban@users.noreply.github.com>
2023-03-22 19:02:13 -0600

    Made updates tomake site public facing
    Created code to hash user passwords
    installed dependancies for heroku
    removed hardcoded port

--- index.js
+++ index.js
@@ -23,12 +23,17 @@ mongoose.connect('mongodb://localhost:27017/[movie', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const cors = require('cors');
+app.use(cors());
+
 let auth = require('./auth')(app);
 
 const passport = require('passport');
 require('./passport');
 
 app.use(morgan('common'));
+
+const { check, validationResult } = require('express-validator');
 
 
 // default text response 
@@ -127,58 +132,96 @@ Birthday: Date
 
 
 //CREATE
-app.post('/users', (req, res) => {
-  console.log(req.body)
-  Users.findOne({ Username: req.body.Username })
-    .then((user) => {
+app.post(
+  '/users',
+  [
+    check('Username', 'Username is required').isLength({ min: 5 }),
+    check(
+      'Username',
+      'Username contains non alphanumeric characters - not allowed.'
+    ).isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail(),
+  ],
+  (req, res) => {
+    let errors = validationResult(req);
 
-      console.log (user)
-      if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
-            } else {
-              Users
-                .create({
-                  Username: req.body.Username,
-                  Password: req.body.Password,
-                  Email: req.body.Email,
-                  Birthday: req.body.Birthday
-                })
-                .then((user) => {res.status(201).json(user) })
-                .catch((error) => {
-                  console.error(error);
-                  res.status(500).send('Error: ' + error);
-                })
-            }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ', error);
-    });
-});
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res
+            .status(400)
+            .send(req.body.Username + 'already exists');
+        } else {
+          Users.create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday,
+          })
+            .then((user) => {
+              res.status(201).json(user);
+            })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).send('Error: ' + error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  }
+);
 
 //UPDATE
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOneAndUpdate(
-    {Username: req.params.Username },
-    {
-      $set: {
-        Username: req.params.Username,
-        Password: req.params.Password,
-        Email: req.params.Email,
-        Birthday: req.params.Birthday,
+app.put(
+  '/users/:Username',
+  [
+    check('Username', 'Username is required').isLength({ min: 5 }),
+    check(
+      'Username',
+      'Username contains non alphanumeric characters - not allowed.'
+    ).isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail(),
+  ],
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOneAndUpdate(
+      { Username: req.params.Username },
+      {
+        $set: {
+          Username: req.body.Username,
+          Password: hashedPassword,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        },
       },
-    },
-    { new: true }, 
-    (err, updatedUser) => {
-      if (err) {
+      { new: true }
+    )
+      .then((updatedUser) => {
+        res.status(200).json(updatedUser);
+      })
+      .catch((err) => {
         console.error(err);
         res.status(500).send('Error: ' + err);
-      } else {
-        res.json(updatedUser);
-      }
-    }
-  );
-});
+      });
+  }
+);
 
 
 //DELETE
@@ -219,8 +262,9 @@ app.get('/secreturl', (req, res) => {
 });
 
 // listen for requests
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
 
 //Handling Errors
--- models.js
+++ models.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
+
 let movieSchema = mongoose.Schema( { 
     Title: {type: String, required: true },
     Description: {type: String, required: true },
@@ -28,3 +30,12 @@ let User = mongoose.model('User', userSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
+
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+  };
+  
+  userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+  };
+
