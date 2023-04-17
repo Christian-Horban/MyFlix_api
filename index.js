79cee6b41ec9ea936f0dac969d238c3a3c91a621
Christian Horban <116985183+Christian-Horban@users.noreply.github.com>
2023-04-17 10:29:42 -0600

    updated cors

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+  origin: ['https://localhost:1234','https://git.heroku.com/horban-movie-api.git', '*']
+}));
 
 let auth = require('./auth')(app);
 
