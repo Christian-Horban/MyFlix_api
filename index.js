8c5f410a2584a3f6b12b5faa91f9ab361a0562a5
Christian Horban <116985183+Christian-Horban@users.noreply.github.com>
2023-04-28 15:04:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -32,18 +32,16 @@ const cors = require('cors');
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://git.heroku.com/horban-movie-api.git'];
 methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'];
 
-app.use(cors());
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   }
-// })
-// .catch( e => console.log(e))
-// );
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app);
 
