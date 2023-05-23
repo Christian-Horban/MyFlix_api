affc21b8c79e15dffc786c2d25a916b098f161e5
Christian Horban <116985183+Christian-Horban@users.noreply.github.com>
2023-05-23 15:11:03 -0600

    removed cors
    removed cors from package.json

--- index.js
+++ index.js
@@ -29,19 +29,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
- let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://git.heroku.com/horban-movie-api.git', "https://myflix-horban.netlify.app"];
- methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'];
+//  let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://git.heroku.com/horban-movie-api.git', "https://myflix-horban.netlify.app"];
+//  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'];
 
 app.use(cors());
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+  // origin: (origin, callback) => {
+  //   if (!origin) return callback(null, true);
+  //   if (allowedOrigins.indexOf(origin) === -1) {
+  //     let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+  //     return callback(new Error(message), false);
+  //   }
+  //   return callback(null, true);
+  // };
 
 let auth = require('./auth')(app);
 
